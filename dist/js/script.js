2cca372e471ed0624af674e091f7d54bf1484ac2
Іван Бесіда <vanya1bes@gmail.com>
2017-09-24 23:05:15 +0300

    minor improvements

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,4 @@
-submitForm = function(){    
+var submitForm = function(){    
     var left_currency_value = document.getElementById('left_value').value,
     left_currency = document.getElementById('left-group').value,
     right_currency = document.getElementById('right-group').value,
@@ -6,29 +6,28 @@ submitForm = function(){
     data,
     sum;
 
-    var xhr = new XMLHttpRequest(),
-    method = "GET",
-    url = "http://api.fixer.io/latest?base=" + left_currency;
-    xhr.open(method, url, true);
-    xhr.onreadystatechange = function () {
-    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-       data = JSON.parse(xhr.responseText);
-
-      sum = left_currency_value * data.rates[right_currency];
-      result.innerHTML = sum.toFixed(4) + ' ' + right_currency;
-      if(left_currency === right_currency){
+    if(left_currency === right_currency){
         result.innerHTML = left_currency_value + ' ' + right_currency;
-      }
+    }else{
+        var xhr = new XMLHttpRequest(),
+        method = "GET",
+        url = "http://api.fixer.io/latest?base=" + left_currency;
+        xhr.open(method, url, true);
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                data = JSON.parse(xhr.responseText);
+                sum = left_currency_value * data.rates[right_currency];
+                result.innerHTML = sum.toFixed(4) + ' ' + right_currency;
+            }
+        };
+        xhr.send();
     }
-    };
-    xhr.send();
-}
+};
 
-reverse = function(){
-    console.log('reverse')
-        var left_currency = document.getElementById('left-group').value,
+var reverse = function(){
+    var left_currency = document.getElementById('left-group').value,
         right_currency = document.getElementById('right-group').value;
-        document.getElementById('left-group').value = right_currency;
-        document.getElementById('right-group').value = left_currency;
-
+        
+    document.getElementById('left-group').value = right_currency;
+    document.getElementById('right-group').value = left_currency;
 };
